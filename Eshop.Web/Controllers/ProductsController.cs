e2d448c8473ec2461f073788937c2e4f8f197a85
LunaDelevska <lunadelevska2002@gmail.com>
2025-02-09 16:31:04 +0100

    Add to shopping cart code adjustments

--- Eshop.Web/Controllers/ProductsController.cs
+++ Eshop.Web/Controllers/ProductsController.cs
@@ -7,16 +7,32 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EShop.Domain.Domain;
 using EShop.Repository;
+using System.Security.Claims;
+using EShop.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Eshop.Web.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IItineraryService _itineraryService;
+        private readonly IPlannedRouteService _plannedRouteService;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(ApplicationDbContext context, IProductService? productService,
+            IShoppingCartService? shoppingCartService,
+             IItineraryService itineraryService,
+             IPlannedRouteService plannedRouteService)
         {
+
             _context = context;
+            _productService = productService;
+            _shoppingCartService = shoppingCartService;
+            _itineraryService = itineraryService;
+            _plannedRouteService = plannedRouteService;
+
         }
 
         // GET: Products
@@ -65,6 +81,27 @@ namespace Eshop.Web.Controllers
             }
             return View(product);
         }
+        [HttpPost]
+        public async Task<IActionResult> AddToCartConfirmed(ProductInShoppingCart model)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "";
+            _shoppingCartService.AddToShoppingConfirmed(model, userId);
+            return View("Index", _productService.GetAllProducts());
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AddToCart(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = _productService.GetDetailsForProduct(id);
+            ProductInShoppingCart ps = new ProductInShoppingCart();
+            ps.ProductId = product.Id;
+            return View(ps);
+        }
+
 
         // GET: Products/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
